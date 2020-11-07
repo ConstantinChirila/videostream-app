cc7e0c72542630740805e29bd2d183bc151166c9
Constantin Chirila <constantin.chirila@gmail.com>
2020-11-07 19:38:42 +0000

    Fixed SVG warnings

--- app/src/components/CallerList/call-list.tsx
+++ app/src/components/CallerList/call-list.tsx
@@ -22,11 +22,11 @@ export function CallerList({ users, yourID, callPeer }: TProps): ReactElement {
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
-              stroke-width="2"
+              strokeWidth="2"
               stroke="currentColor"
               fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M0 0h24v24H0z" stroke="none" />
               <path d="M5 4h4l2 5-2.5 1.5a11 11 0 005 5L15 13l5 2v4a2 2 0 01-2 2A16 16 0 013 6a2 2 0 012-2M15 7a2 2 0 012 2M15 3a6 6 0 016 6" />
