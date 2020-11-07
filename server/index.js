ab2e2614d1beb49eb5ce4e3bd4ba48f076f7c5f7
Constantin Chirila <constantin.chirila@gmail.com>
2020-11-07 16:47:59 +0000

    Fixed Cors issue on the Server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
-const express = require("express");
-const http = require("http");
-const app = express();
-const server = http.createServer(app);
+const app = require("express")();
+const server = require("http").createServer(app);
 const socket = require("socket.io");
-const io = socket(server);
+const io = socket(server, { origins: "*:*" });
 
 const users = {};
 
